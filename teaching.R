5c402e6a69e35b862b2688572a25e65b89a7890f
rafceegee <35352704+rafceegee@users.noreply.github.com>
2025-02-06 15:57:46 -0500

    CV

---
+++ teaching.R
@@ -0,0 +1,8 @@
+---
+  title: "Teaching"
+---
+  
+During my graduate 
+
+### Courses I have TAed
+- “Housing, Healthcare, and More: Decoding the 2022 Ontario General Election” Under Review in **The Canadian Political Science Review**, with Andrea Perrella & Simon Kiss.
