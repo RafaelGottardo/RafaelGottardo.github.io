593b8bb96c0d78de2dd40d2dac0bf28ab2379502
rafceegee <35352704+rafceegee@users.noreply.github.com>
2026-08-18 10:03:55 -0400

    updated so it is only changes

--- load_data.R
+++ load_data.R
@@ -2,12 +2,12 @@
 toronto_permits <- read.csv("data/data_raw/Building_permit_data/Cleared Building Permits since 2017.csv")
 
 toronto_permits <- toronto_permits %>% 
-  mutate(change = ifelse(PROPOSED_USE == CURRENT_USE, 0, 1))
+  mutate(change = ifelse(PROPOSED_USE == CURRENT_USE | str_detect(PROPOSED_USE, "Same"), 0, 1))
 
 toronto_permits_change <- toronto_permits %>% 
   filter(change == 1)
 
-toronto_permits <- toronto_permits %>% 
+toronto_permits_change <- toronto_permits_change %>% 
   mutate(Address = paste(STREET_NUM, STREET_NAME, STREET_TYPE),
          Address = str_to_title(Address))
 
@@ -16,21 +16,21 @@ Toronto_address_points <- read_sf("data/data_raw/Address Points - 4326.geojson")
 Toronto_address_points <- Toronto_address_points %>%
   distinct(ADDRESS_FULL, .keep_all = TRUE)
 
-toronto_permits <- toronto_permits %>% 
+toronto_permits_change <- toronto_permits_change %>% 
   left_join(Toronto_address_points %>%  select(ADDRESS_FULL, geometry),
             by = c("Address" = "ADDRESS_FULL")) %>% 
   st_as_sf()
 
 
-toronto_permits <- st_transform(toronto_permits, crs = 4326)
+toronto_permits_change <- st_transform(toronto_permits_change, crs = 4326)
 
 
-toronto_permits <- toronto_permits %>%
+toronto_permits_change <- toronto_permits_change %>%
   filter(!st_is_empty(geometry)) %>%   # drop the 82,484 unmatched rows
   st_cast("POINT")                      # MULTIPOINT -> POINT
 
 
-toronto_permits <- toronto_permits %>% 
+toronto_permits_change <- toronto_permits_change %>% 
   mutate(pop_up = paste0("<b>Permit Type:</b> ", PERMIT_TYPE, "<br>",
                          "<b>Structure Type:</b> ", STRUCTURE_TYPE, "<br>",
                          "<b>Current Use:</b> ", CURRENT_USE,  "<br>",
@@ -39,7 +39,7 @@ toronto_permits <- toronto_permits %>%
                          "<b>Description:</b> ", DESCRIPTION,  "<br>"
   ))
 
-toronto_permits_2021 <- toronto_permits %>% 
+toronto_permits_2021 <- toronto_permits_change %>% 
   mutate(APPLICATION_DATE = as.Date(APPLICATION_DATE)) %>% 
   filter(APPLICATION_DATE >= "2020-01-01" & APPLICATION_DATE <= "2022-12-31")
 
